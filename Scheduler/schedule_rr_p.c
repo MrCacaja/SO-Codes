19804c96af3594a739a10c913ad24d9e0d4b674e
Carlos Eduardo Prezzi <carlos.prezzi@be220.com>
2023-05-27 15:32:54 -0300

    Implementação básica de round-robin com prioridade

--- Scheduler/schedule_rr_p.c
+++ Scheduler/schedule_rr_p.c
@@ -1,11 +1,63 @@
 #include "schedule_rr.h"
+#include "list.h"
+#include "task.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "CPU.h"
 
-// add a task to the list 
+#define MAX_STACKS 10
+
+struct node *stack[MAX_STACKS];
+int quantum = 30;
+
+// add a task to the list
 void add(char *name, int priority, int burst){
-   int x = 0;
+    struct task *task = malloc(sizeof(struct task));
+    task->name = name;
+    task->priority = priority;
+    task->burst = burst;
+    
+    push(&stack[priority - 1], task);
 }
 
 // invoke the scheduler
 void schedule(){
-   int x = 0;
+    for (int i = 0; i < MAX_STACKS; i++) {
+        if (stack[i] == 0) {
+            stack[i] = malloc(sizeof (struct node));
+            continue;
+        }
+        while (stack[i]->next) {
+            struct node *next = stack[i]->next;
+            if (stack[i]->task->burst > quantum) {
+                run(stack[i]->task, quantum);
+                stack[i]->task->burst -= quantum;
+                stack[i]->task->priority++;
+                push(&stack[i + 1], stack[i]->task);
+                free(stack[i]);
+            } else {
+                run(stack[i]->task, stack[i]->task->burst);
+                free(stack[i]);
+            }
+            stack[i] = next;
+        }
+        if (stack[i]->task->burst > quantum) {
+            if (i == MAX_STACKS - 1) {
+                while (stack[i]->task->burst > quantum) {
+                    run(stack[i]->task, quantum);
+                    stack[i]->task->burst -= quantum;
+                }
+                run(stack[i]->task, stack[i]->task->burst);
+            } else {
+                run(stack[i]->task, quantum);
+                stack[i]->task->burst -= quantum;
+                stack[i]->task->priority++;
+                push(&stack[i + 1], stack[i]->task);
+            }
+        } else {
+            run(stack[i]->task, stack[i]->task->burst);
+        }
+        free(stack[i]);
+    }
 }
